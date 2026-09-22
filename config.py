cbcda7d865a279dfa41c55893cdc52474dc2fab8
sospu07-debug <sospu07@gmail.com>
2026-09-22 21:33:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+API_KEY = (
+    'Te2SJU8b6uhGXjWaN3gijm3FBgUQ'
+    'TlqOrVzMMzH8GElgvsnjPtpXy3DPD'
+    'mdJHfNN'
+)
+
+API_SECRET = (
+    '0BKIJKOC9KhqnXaZCstBfc4JnuvUF'
+    '2L0d7BvA3YaxyJHvSOniuksb1Th1ra'
+    'M2Qaz'
+)
+
+TELEGRAM_TOKEN = (
+    '8911308822'
+    'AAH4EPvsJzoXbG7iCAfq1t_a_sswQf2RqRY'
+)
